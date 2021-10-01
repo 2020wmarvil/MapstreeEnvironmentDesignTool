fb020ef9cdead5c3e1d866182e6b26e6c1336808
Wyatt Marvil <2020wmarvil@gmail.com>
2021-10-01 12:53:20 -0400

    Initialize UE4 Project

---
+++ Mapstree/Source/Mapstree/Mapstree.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Mapstree.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Mapstree, "Mapstree" );
---
+++ Mapstree/Source/Mapstree/MapstreeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MapstreeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAPSTREE_API AMapstreeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
